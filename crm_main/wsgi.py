b850cb7ed66098a4bd00abd822386fff5c9920de
asaprockky <shokirovabdulfayz@gmail.com>
2025-03-26 19:35:54 +0500

    Configured some files

--- crm_main/wsgi.py
+++ crm_main/wsgi.py
@@ -4,4 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_main.settings')
 
 # Vercel requires a named 'app' variable
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
